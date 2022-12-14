9b4ebb7bcc4927fe78eaca76c6eae119524664c4
Sujeong, Oh <89963974+5sudeng@users.noreply.github.com>
2022-12-14 10:15:52 +0900

    Create OX.c
    2일차 OX퀴즈 문제풀이 입니다.

---
+++ assignment/OX.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    char arr[80];
+    scanf("%d", &n);
+    
+    for(int i=0;i<n;i++){
+        scanf("%s", arr);
+        int correct = 1;
+        int score = 0;
+        for(int i=0;arr[i]!=0;i++){
+            if (arr[i] == 'O'){
+                score += correct;
+                correct++;
+            } else {
+                correct = 1;
+            }
+        }
+        printf("%d\n", score);
+    }
+    
+    return 0;
+}
